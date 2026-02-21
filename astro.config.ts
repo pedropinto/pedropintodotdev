133ad258b227810586cd1617119f65fe979b587e
Pedro Pinto <pmsilpinto@gmail.com>
2026-02-21 14:53:01 +0000

    Updated site URL to pedropinto.dev

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://pedropintodotdev.com",
+  site: "https://pedropinto.dev",
   vite: {
     plugins: [tailwindcss()],
   },
